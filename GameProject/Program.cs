21296ba165b571279e3a3f8465997b5ba6a029e9
uurmtn <oxir47@gmail.com>
2021-01-30 15:56:17 +0300

    Add project files.

---
+++ GameProject/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 1991,
+                FirstName = "UĞUR",
+                LastName = "METİN",
+                IdentityNumber = 12345
+
+            });
+            gamerManager.Delete(new Gamer
+            {
+                Id = 1,
+                BirthYear = 1991,
+                FirstName = "UĞUR",
+                LastName = "METİN",
+                IdentityNumber = 12345
+
+            });
+            gamerManager.Update(new Gamer
+            {
+                Id = 1,
+                BirthYear = 1991,
+                FirstName = "UĞUR",
+                LastName = "METİN",
+                IdentityNumber = 12345
+
+            });
+
+
+        }
+    }
+}
